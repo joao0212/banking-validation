7fbe2ced684d5680379e45c677eeaa9a511def08
Joao Victor <jvc.martins12@gmail.com>
2025-12-10 23:39:57 -0300

    producer terceira aula

--- src/main/java/br/com/alura/service/SituacaoCadastralService.java
+++ src/main/java/br/com/alura/service/SituacaoCadastralService.java
@@ -3,8 +3,11 @@ package br.com.alura.service;
 import br.com.alura.domain.Agencia;
 import br.com.alura.domain.audit.Audit;
 import br.com.alura.repository.SituacaoCadastralRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.MutinyEmitter;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
@@ -16,9 +19,19 @@ public class SituacaoCadastralService {
 
     private final Emitter<Audit> emitter;
 
-    public SituacaoCadastralService(SituacaoCadastralRepository situacaoCadastralRepository, @Channel("notificacoes") Emitter<Audit> emitter) {
+    private final MutinyEmitter<String> kafkaEmitter;
+
+    private final ObjectMapper objectMapper;
+
+    public SituacaoCadastralService(
+            SituacaoCadastralRepository situacaoCadastralRepository,
+            @Channel("notificacoes") Emitter<Audit> emitter,
+            @Channel("remover-agencia-channel") MutinyEmitter<String> kafkaEmitter
+    ) {
         this.situacaoCadastralRepository = situacaoCadastralRepository;
         this.emitter = emitter;
+        this.kafkaEmitter = kafkaEmitter;
+        this.objectMapper = new ObjectMapper();
     }
 
     @WithTransaction
@@ -26,10 +39,14 @@ public class SituacaoCadastralService {
         return situacaoCadastralRepository
                 .update("situacaoCadastral = ?1 where cnpj = ?2",
                         agencia.getSituacaoCadastral(), agencia.getCnpj())
-                .onItem().invoke(a -> {
-                    // envio "fire-and-forget"
-                    emitter.send(new Audit(agencia.getId(), agencia.getCnpj(), agencia.getSituacaoCadastral()));
-                })
-                .replaceWithVoid();
+                .onItem()
+                .invoke(() -> emitter.send(new Audit(agencia.getId(), agencia.getCnpj(), agencia.getSituacaoCadastral())))
+                .call(() -> {
+                    try {
+                        return kafkaEmitter.send(objectMapper.writeValueAsString(agencia));
+                    } catch (JsonProcessingException e) {
+                        return Uni.createFrom().failure(e);
+                    }
+                }).replaceWithVoid();
     }
 }
